bd56803a0cc7760182b572a68748a5c850693e2d
rakeshcooper <rakeshmurali097@gmail.com>
2024-12-29 00:20:29 +0530

    mongoose implemented

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express"
+import colors from "colors"
 import errorhandler from "./middleware/errorMiddleware.js"
+import connectDB from "./config/db.js"
 import dotenv  from "dotenv"
 dotenv.config()
 import router from "./routes/goalRoutes.js"
 
 
 const port = process.env.PORT || 5000
+
+connectDB()
+
 const app = express()
 
 //Body parser middleware
