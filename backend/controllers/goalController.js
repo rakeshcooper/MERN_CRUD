56e48b8f5b9711e75a7b5f3437caca90785005f7
rakeshcooper <rakeshmurali097@gmail.com>
2024-12-28 20:59:47 +0530

    Implementing route

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -18,6 +18,11 @@ export const getGoals = (req,res) => {
 // @route POST /api/goals
 // @access private 
 export const setGoals = (req,res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')    
+    }
+    
     res.status(200).json(posts)
 }
 
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,15 +2,7 @@ import express from "express"
 import { getGoals,setGoals, updateGoals, removeGoals } from "../controllers/goalController.js"
 const router = express.Router()
 
-
-
-
-router.get('/', getGoals)
-
-router.post('/',setGoals)
-
-router.put('/:id',updateGoals)
-
-router.delete('/:id',removeGoals)
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').put(updateGoals).delete(removeGoals)
 
 export default router
